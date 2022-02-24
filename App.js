bda118dc4cae2cbd83cd49222708c934cb099543
Varsni-S <varsu84@gmail.com>
2022-02-24 20:33:49 +0530

    animation

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import {View, Text} from 'react-native';
 import React from 'react';
-import Calculator from './src/screens/Calculator';
-import HookForm from './src/screens/HookForm';
+// import Calculator from './src/screens/Calculator';
+// import HookForm from './src/screens/HookForm';
+import Translation from './src/screens/Translation';
 
 export default function App() {
   return (
     <View>
-      <Calculator />
+      {/* <Calculator /> */}
       {/* <HookForm/> */}
+      <Translation />
     </View>
   );
 }
---
+++ src/screens/Translation.js
@@ -0,0 +1,27 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import Header from '../screens/Header';
+import LottieView from 'lottie-react-native';
+
+export default function Translation() {
+  return (
+    <View>
+      <Header name="Translation" />
+
+      <LottieView
+        source={require('../../assests/97039-oh-no.json')}
+        style={styles.animation}
+        speed={3}
+        autoPlay
+        loop={true}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  animation: {
+    width: 200,
+    height: 300,
+  },
+});
