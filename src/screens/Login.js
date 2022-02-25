7936b541346115f6cb773f928bfc69795c5dacc3
Varsni-S <varsu84@gmail.com>
2022-02-25 14:51:56 +0530

    header

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -8,7 +8,6 @@ import {
 } from 'react-native';
 import React from 'react';
 import Header from '../components/Header';
-//import Header from './Header';
 
 export default function Login() {
   return (
