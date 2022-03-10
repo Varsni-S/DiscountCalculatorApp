48856cac52321b6531aeb504158ac0e0d71b4586
Varsni-S <varsu84@gmail.com>
2022-03-10 15:04:54 +0530

    api call

--- App.js
+++ App.js
@@ -2,11 +2,13 @@ import {View, Text} from 'react-native';
 import React from 'react';
 
 import Realm from './src/screens/Realm';
+import Api from './src/screens/Api';
 
 export default function App() {
   return (
     <View>
-      <Realm />
+      {/* <Realm /> */}
+      <Api />
     </View>
   );
 }
